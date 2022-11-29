376c809508e29ff90d2b129e5832111314988e55
dann <danil.mekh@mail.ru>
2022-11-29 17:32:11 +0700

    as

---
+++ polls/urls.py
@@ -0,0 +1,16 @@
+from .views import LogoutView
+from django.urls import path
+from .views import profile
+from . import views
+
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('register/', views.register, name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+]
---
+++ polls/views.py
@@ -0,0 +1,72 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+from .models import Question, Choice
+from django.template import loader
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/basic.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегестрировались')
+            return redirect('/login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'polls/register.html', {"form": form})
+
+
+class LoginView(LoginView):
+    template_name = 'polls/login.html'
+    success_url = reverse_lazy('polls/profile')
+
+
+@login_required
+def profile(request):
+    return render(request, 'polls/profile.html')
+
+
+class LogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'polls/logout.html'
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'вы не сделали выбор'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
